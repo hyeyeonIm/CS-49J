51275f9cadbbb7e42bdb85615e3cdf60aebe264c
hyeyeonIm <enhyeonii@gmail.com>
2022-09-20 00:39:10 -0700

    CS49J homework3

---
+++ type-search-index.js
@@ -0,0 +1,1 @@
+typeSearchIndex = [{"l":"All Classes and Interfaces","u":"allclasses-index.html"},{"p":"<Unnamed>","l":"Main"},{"p":"edu.sjsu.assignment2","l":"MyFile"},{"p":"edu.sjsu.assignment2","l":"ScanMyTime"}];updateSearchResults();
